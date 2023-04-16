71ae89e930325a913f97992b51399e719a887f74
heymanolito <manuelgallardoffs@gmail.com>
2023-04-16 13:24:19 +0200

    landing joao

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const sr = ScrollReveal({
+    origin: 'bottom',
+    distance: '20px',
+    duration: 900,
+    delay: 0,
+    reset: true
+  });
+
+
+
+sr.reveal('#who-am-i img', { delay: 200 });
+sr.reveal('.section-title', { delay: 200 });
+sr.reveal('.section-description', { delay: 200 });
+sr.reveal('.card', { interval: 200 });
+sr.reveal('#contact', { interval: 200 });
